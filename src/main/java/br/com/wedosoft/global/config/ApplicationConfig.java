ab659ab121000f5f09dbb55866e67f5646a8bead
Rodolfo Faquin <rodolfofaquin@gmail.com>
2018-05-11 20:28:19 -0300

    Commit inicial

--- src/main/java/br/com/wedosoft/global/config/ApplicationConfig.java
+++ src/main/java/br/com/wedosoft/global/config/ApplicationConfig.java
@@ -6,30 +6,4 @@ import javax.ws.rs.core.Application;
 @ApplicationPath("/api")
 public class ApplicationConfig extends Application {
 
-/*
-    public ApplicationConfig() {
-        BeanConfig beanConfig = new BeanConfig();
-        beanConfig.setVersion("1.0.2");
-        beanConfig.setSchemes(new String[]{"http"});
-        beanConfig.setHost("localhost:8080");
-        beanConfig.setBasePath("/doc");
-        beanConfig.setResourcePackage("me.fiquefit.resources");
-        beanConfig.setScan(true);
-    }
-
-    @Override
-    public Set<Class<?>> getClasses() {
-        Set<Class<?>> resources = new HashSet<>();
-
-        resources.add(TituloResource.class);
-        //resources.add(FirstResource.class);
-        //resources.add(SecondResource.class);
-        //...
-
-        resources.add(io.swagger.jaxrs.listing.ApiListingResource.class);
-        resources.add(io.swagger.jaxrs.listing.SwaggerSerializers.class);
-
-        return resources;
-    }
-    */
 }
